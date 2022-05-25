770baa7b6f0b2847091e5110414ad2af4d8513ac
Arjun Gupta <arjuntheprogrammer@gmail.com>
2022-05-25 11:51:06 +0530

    retain state

--- src/extension.ts
+++ src/extension.ts
@@ -1,70 +1,165 @@
 import * as vscode from "vscode";
+import * as path from "path";
 
-const cats = {
-  "Coding Cat": "https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif",
-  "Compiling Cat": "https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif",
-  "Testing Cat": "https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif",
-};
+// const cats = {
+//   "Coding Cat": "https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif",
+//   "Compiling Cat": "https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif",
+//   "Testing Cat": "https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif",
+// };
 
 export function activate(context: vscode.ExtensionContext) {
-  // Track currently webview panel
+  // Only allow a single Cat Coder
   let currentPanel: vscode.WebviewPanel | undefined = undefined;
 
-  let disposable = vscode.commands.registerCommand("catCoding.start", () => {
-    const panel = vscode.window.createWebviewPanel(
-      "catCoding",
-      "Cat Coding",
-      vscode.ViewColumn.One,
-      {}
-    );
-    panel.webview.html = getWebviewContent("Coding Cat");
+  context.subscriptions.push(
+    vscode.commands.registerCommand("catCoding.start", () => {
+      if (currentPanel) {
+        currentPanel.reveal(vscode.ViewColumn.One);
+      } else {
+        currentPanel = vscode.window.createWebviewPanel(
+          "catCoding",
+          "Cat Coding",
+          vscode.ViewColumn.One,
+          {
+            enableScripts: true,
+          }
+        );
+        currentPanel.webview.html = getWebviewContent();
 
-    // Update contents based on view state changes
-    panel.onDidChangeViewState(
-      (e) => {
-        const panel = e.webviewPanel;
-        switch (panel.viewColumn) {
-          case vscode.ViewColumn.One:
-            updateWebviewForCat(panel, "Coding Cat");
-            return;
+        currentPanel.onDidDispose(
+          () => {
+            currentPanel = undefined;
+          },
+          undefined,
+          context.subscriptions
+        );
 
-          case vscode.ViewColumn.Two:
-            updateWebviewForCat(panel, "Compiling Cat");
-            return;
+        // Handle messages from the webview
+        currentPanel.webview.onDidReceiveMessage(
+          (message) => {
+            switch (message.command) {
+              case "alert":
+                vscode.window.showErrorMessage(message.text);
+                return;
+            }
+          },
+          undefined,
+          context.subscriptions
+        );
+      }
+    })
+  );
 
-          case vscode.ViewColumn.Three:
-            updateWebviewForCat(panel, "Testing Cat");
-            return;
-        }
-      },
-      null,
-      context.subscriptions
-    );
-  });
+  // Our new command
+  context.subscriptions.push(
+    vscode.commands.registerCommand("catCoding.doRefactor", () => {
+      if (!currentPanel) {
+        return;
+      }
 
-  context.subscriptions.push(disposable);
+      // Send a message to our webview.
+      // You can send any JSON serializable data.
+      currentPanel.webview.postMessage({ command: "refactor" });
+    })
+  );
+
+  // And make sure we register a serializer for our webview type
+  vscode.window.registerWebviewPanelSerializer(
+    "catCoding",
+    new CatCodingSerializer()
+  );
 }
 
 export function deactivate() {}
 
-function updateWebviewForCat(
-  panel: vscode.WebviewPanel,
-  catName: keyof typeof cats
-) {
-  panel.title = catName;
-  panel.webview.html = getWebviewContent(catName);
+class CatCodingSerializer implements vscode.WebviewPanelSerializer {
+  async deserializeWebviewPanel(webviewPanel: vscode.WebviewPanel, state: any) {
+    // `state` is the state persisted using `setState` inside the webview
+    console.log(`Got state: ${state}`);
+
+    // Restore the content of our webview.
+    //
+    // Make sure we hold on to the `webviewPanel` passed in here and
+    // also restore any event listeners we need on it.
+    webviewPanel.webview.html = getWebviewContent();
+  }
 }
 
-function getWebviewContent(cat: keyof typeof cats) {
+// function updateWebviewForCat(
+//   panel: vscode.WebviewPanel,
+//   catName: keyof typeof cats
+// ) {
+//   panel.title = catName;
+//   panel.webview.html = getWebviewContent(catName);
+// }
+
+// function getWebviewContent(cat: keyof typeof cats) {
+//   return `<!DOCTYPE html>
+//   <html lang="en">
+//   <head>
+// 	  <meta charset="UTF-8">
+// 	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+// 	  <title>Cat Coding</title>
+//   </head>
+//   <body>
+// 	  <img src="${cats[cat]}" width="300" />
+//   </body>
+//   </html>`;
+// }
+// function getWebviewContent(cat: any) {
+//   return `<!DOCTYPE html>
+//   <html lang="en">
+//   <head>
+// 	  <meta charset="UTF-8">
+// 	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+// 	  <title>Cat Coding</title>
+//   </head>
+//   <body>
+// 	  <img src="${cat}" width="300" />
+//   </body>
+//   </html>`;
+// }
+
+function getWebviewContent() {
   return `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-	  <meta charset="UTF-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	  <title>Cat Coding</title>
-  </head>
-  <body>
-	  <img src="${cats[cat]}" width="300" />
-  </body>
-  </html>`;
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+    <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+    <h1 id="lines-of-code-counter">0</h1>
+
+    <script>
+        (function() {
+            const vscode = acquireVsCodeApi();
+            const counter = document.getElementById('lines-of-code-counter');
+
+            // Check if we have an old state to restore from
+            const previousState = vscode.getState();
+            let count = previousState ? previousState.count : 0;
+            counter.textContent = count;
+
+            setInterval(() => {
+              counter.textContent = count++;
+              // Update the saved state
+              vscode.setState({ count });
+            }, 100);
+
+            // setInterval(() => {
+            //     counter.textContent = count++;
+            //     // Alert the extension when our cat introduces a bug
+            //     if (Math.random() < 0.001 * count) {
+            //         vscode.postMessage({
+            //             command: 'alert',
+            //             text: '🐛  on line ' + count
+            //         })
+            //     }
+            // }, 100);
+        }())
+    </script>
+</body>
+</html>`;
 }
